82357eab57edafb7b0edfefcac91cae7a865c65c
satya <satya143@gmail.com>
2023-04-16 14:27:38 +0530

    Add:checking valid if one Special chracter is there

--- src/main/java/com/bridgelabz/userRegistrationProblem/CheckRegularExpressionPattern.java
+++ src/main/java/com/bridgelabz/userRegistrationProblem/CheckRegularExpressionPattern.java
@@ -6,17 +6,17 @@ import java.util.regex.Pattern;
 
 public class CheckRegularExpressionPattern {
 
-    public void checkPassword() {
+    public void checkOneSpecialChracterPassword() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your password : ");
+        System.out.println("Enter your password one upper case one number and a Secial  chracter password : ");
         String password = scanner.next();
-        String regex = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+        String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).{8,}$";
         //Creating a pattern object
         Pattern pattern = Pattern.compile(regex);
         //Creating a Matcher object
         Matcher matcher = pattern.matcher(password);
         if (matcher.matches()) {
-            System.out.println("Given at least one upper case and one number in password  is valid");
+            System.out.println("Given  password  is valid");
         } else {
             System.out.println("Given password  is not valid");
         }
@@ -24,7 +24,7 @@ public class CheckRegularExpressionPattern {
 
     public static void main(String[] args) {
         CheckRegularExpressionPattern search = new CheckRegularExpressionPattern();
-        search.checkPassword();
+        search.checkOneSpecialChracterPassword();
 
     }
 }
