49c9b9b9d82b98137c7f99b3fb7e7b1f38ffcca1
satya <satya143@gmail.com>
2023-04-16 11:02:54 +0530

    Add:Check the valid last name

--- src/main/java/com/bridgelabz/userRegistrationProblem/CheckRegularExpressionPattern.java
+++ src/main/java/com/bridgelabz/userRegistrationProblem/CheckRegularExpressionPattern.java
@@ -6,18 +6,18 @@ import java.util.regex.Pattern;
 
 public class CheckRegularExpressionPattern {
 
-    public void checkRegularExpPattern() {
+    public void ValidUserLastNameCheck() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name = sc.nextLine();
+        System.out.println("Enter your last name: ");
+        String lastName = sc.nextLine();
         Pattern p = Pattern.compile("[a-zA-Z]{3,}");
-        Matcher m = p.matcher(name);
+        Matcher m = p.matcher(lastName);
         boolean b = m.matches();
         System.out.println(b);
     }
     public static void main(String[] args) {
-        CheckRegularExpressionPattern  search = new CheckRegularExpressionPattern();
-        search.checkRegularExpPattern();
+        CheckRegularExpressionPattern search = new CheckRegularExpressionPattern();
+        search.ValidUserLastNameCheck();
 
     }
 }
