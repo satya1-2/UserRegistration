23d5eb09875dad86c4f815a1f4aca1138f685b80
satya <satya143@gmail.com>
2023-04-16 13:50:55 +0530

    Add:Check the valid phone number

--- src/main/java/com/bridgelabz/userRegistrationProblem/CheckRegularExpressionPattern.java
+++ src/main/java/com/bridgelabz/userRegistrationProblem/CheckRegularExpressionPattern.java
@@ -6,20 +6,26 @@ import java.util.regex.Pattern;
 
 public class CheckRegularExpressionPattern {
 
-    public void ValidUserLastNameCheck() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your last name: ");
-        String lastName = sc.nextLine();
-        Pattern p = Pattern.compile("[a-zA-Z]{3,}");
-        Matcher m = p.matcher(lastName);
-        boolean b = m.matches();
-        System.out.println(b);
-    }
-    public static void main(String[] args) {
-        CheckRegularExpressionPattern search = new CheckRegularExpressionPattern();
-        search.ValidUserLastNameCheck();
+    public void validUserPhoneNumberCheck() {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter your phone number: ");
+            String phoneNumber = scanner.next();
+            String regex = "^[+]{1}(?:[0-9\\-\\(\\)\\/\\.]\\s?){6,15}[0-9]{1}$";
+            //Creating a pattern object
+            Pattern pattern = Pattern.compile(regex);
+            //Creating a Matcher object
+            Matcher matcher = pattern.matcher(phoneNumber);
+            if (matcher.matches()) {
+                System.out.println("Given phone number  is valid");
+            } else {
+                System.out.println("Given phone number  is not valid");
+            }
+        }
 
-    }
-}
+        public static void main(String[] args) {
+            CheckRegularExpressionPattern search = new CheckRegularExpressionPattern();
+            search.validUserPhoneNumberCheck();
 
+        }
+    }
 
