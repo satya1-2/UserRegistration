bb50c2c61a0741ffddd287c04a40e57552a675ff
satya <satya143@gmail.com>
2023-04-16 13:58:38 +0530

    Add:Check the valid email id

--- src/main/java/com/bridgelabz/userRegistrationProblem/CheckRegularExpressionPattern.java
+++ src/main/java/com/bridgelabz/userRegistrationProblem/CheckRegularExpressionPattern.java
@@ -6,26 +6,28 @@ import java.util.regex.Pattern;
 
 public class CheckRegularExpressionPattern {
 
-    public void validUserPhoneNumberCheck() {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter your phone number: ");
-            String phoneNumber = scanner.next();
-            String regex = "^[+]{1}(?:[0-9\\-\\(\\)\\/\\.]\\s?){6,15}[0-9]{1}$";
-            //Creating a pattern object
-            Pattern pattern = Pattern.compile(regex);
-            //Creating a Matcher object
-            Matcher matcher = pattern.matcher(phoneNumber);
-            if (matcher.matches()) {
-                System.out.println("Given phone number  is valid");
-            } else {
-                System.out.println("Given phone number  is not valid");
-            }
-        }
+    public void validUserEmailCheck() {
 
-        public static void main(String[] args) {
-            CheckRegularExpressionPattern search = new CheckRegularExpressionPattern();
-            search.validUserPhoneNumberCheck();
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your email id: ");
+        String gmail = scanner.next();
+        String regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+        //Creating a pattern object
+        Pattern pattern = Pattern.compile(regex);
+        //Creating a Matcher object
+        Matcher matcher = pattern.matcher(gmail);
+        if (matcher.matches()) {
+            System.out.println("Given email id is valid");
+        } else {
+            System.out.println("Given email id is not valid");
         }
     }
+
+
+    public static void main(String[] args) {
+        CheckRegularExpressionPattern search = new CheckRegularExpressionPattern();
+        search.validUserEmailCheck();
+
+    }
+}
 
